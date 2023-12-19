0a087549b252372ba8c6e8b0a628526657ea364d
Ashwani dubey <ashwanidubey@Ashwanis-MacBook-Air.local>
2023-12-19 15:49:36 +0530

    added 19 dec 2023

--- backend/index.js
+++ backend/index.js
@@ -8,23 +8,48 @@ const server = http.createServer(app);
 const io = socketIO(server);
 const {router}=require('./router/myrouter')
 app.use(express.static('frontend'));
-//app.use(router)
+app.use(router)
 var names={}
+var ids={}
+
 io.on('connection', (socket) => {
-    console.log('A user connected');
-    /*socket.on('user connected', (name) => {
-        console.log('message: ' + name+socket.id);
-         names[socket.id]=name
-       // console.log(names[socket.id])
-    });*/
-    /*socket.on('chat message', (msg) => { 
-        io.emit('chat message', { name: names[socket.id], message: msg });
-    });
-    // Handle disconnection
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });*/
+   
+    socket.on('user connected', (name) => {
+        console.log('message: ' + name+" "+socket.id);
+        io.emit('user connected',name)
+        names[socket.id]=name
+        ids[name]=socket.id
+        
+   });
+   socket.on('chat message', (msg) => { 
+    console.log(msg)
+    console.log("by:",  socket.id)
+    let from=names[socket.id]
+    if(from=='A')
+    {
+        io.to(ids['B']).emit("chat message",{ 
+            name: names[socket.id], message: msg })
+    }
+    else if(from=='B')
+    {
+        io.to(ids['C']).emit("chat message",{
+             name: names[socket.id], message: msg })
+    }
+    else{
+        io.to(ids['A']).emit("chat message",{ 
+            name: names[socket.id], message: msg }) 
+    }
+    
+   });
+   socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 });
+
+ 
+    // Handle disconnection
+
+
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
--- frontend/client.js
+++ frontend/client.js
@@ -1,4 +1,4 @@
-//const socket = io();
+const socket = io();
 const nameContainer = document.getElementById('name-container');
 const nameInput = document.getElementById('name');
 const chatContainer = document.getElementById('container');
@@ -8,7 +8,8 @@ const messages = document.querySelector('#messages');
 function submitName(){
   const name = nameInput.value.trim();
   if (name) {
-    //socket.emit('user connected', name);
+   // socket.id=name
+    socket.emit('user connected', name);
     nameContainer.style.display = 'none';
     chatContainer.style.display = 'block';
   }
@@ -17,21 +18,21 @@ function sendMessage(){
   
   console.log("clicked")
   if (input.value) {
-   //socket.emit('chat message', input.value);
+    socket.emit('chat message', input.value);
     input.value = '';
   }
 }
 
 
-/*
+
   socket.on('user connected', (name) => {
     const li = document.createElement('li');
     li.textContent = `${name} has joined the chat`;
     messages.appendChild(li);
-  });*/
-  /*socket.on('chat message', (data) => {
+  });
+  socket.on('chat message', (data) => {
     const li = document.createElement('li');
-    li.textContent = `${data.name}: ${data.message}`;
+    li.textContent = `${data.name}: ${data.message} `;
     messages.appendChild(li);
-  });*/
+  });
 
